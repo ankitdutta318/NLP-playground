fe3095732c66a9e038799698b83889a812727833
cruxbreaker <geekankit318@gmail.com>
2018-05-30 13:22:42 +0530

    Added stopword.txt file

---
+++ sentiment_analyzer/sentiment.py
@@ -0,0 +1,10 @@
+"""
+    Author: Ankit Dutta <cruxbeaker>
+    Naive Bayes spam detection for NLP
+    dataset: https://archive.ics.uci.edu/ml/datasets/Spambase
+"""
+
+from __future__ import print_function, division
+from future.utils import iteritems
+from builtins import range
+
